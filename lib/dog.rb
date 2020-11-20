7dbf8713d649313d548b0c6087833ffc17873976
David Gomara <davidgomara7@gmail.com>
2020-11-20 21:29:03 +0000

    commit tree d707dbb81a60e76bc0b29da6289525a3d09d3599

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+    
+    attr_accessor :name
+    
+    @@all = []
+    
+    def initialize(name)
+        @name = name 
+        save
+    end 
+    
+    def self.all
+        @@all
+    end 
+    
+    def self.clear_all
+        @@all = []
+    end 
+    
+    def self.print_all
+        print @@all.collect {|dog| dog.name}.join("\n") + "\n"
+    end 
+      
+    def save
+        @@all << self
+    end 
+    
+end 
